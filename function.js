b03da2a1d91395bb3b30bdec6c4eed6d20ac19f5
labibkhanofficialbd <labibkhan66410@gmail.com>
2021-08-04 20:23:04 +0600

    parameter.js

---
+++ function.js
@@ -0,0 +1,22 @@
+
+// function declearation
+
+function startFan(){
+    console.log('start the fan');
+    console.log('gorom lage beta');
+}
+
+// call the function
+
+startFan();
+
+
+
+console.log('eat breakfast');
+console.log('Drink tea');
+console.log('take a shower');
+// startFan();
+var country = "bd";
+var city = 'Dhaka';
+var location = 'Gulisthan';
+startFan();
---
+++ parameter.js
@@ -0,0 +1,8 @@
+
+
+function bringSingara(taka){
+    console.log('singarar jonno taka dise', taka);
+    console.log('mama singara den');
+}
+
+bringSingara(100);

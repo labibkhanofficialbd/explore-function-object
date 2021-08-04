8dcc19d7cf5ecdaae9f12d5bcbce915d8df554e7
labibkhanofficialbd <labibkhan66410@gmail.com>
2021-08-04 20:41:04 +0600

    parameter.js

--- parameter.js
+++ parameter.js
@@ -3,6 +3,13 @@
 function bringSingara(taka){
     console.log('singarar jonno taka dise', taka);
     console.log('mama singara den');
+
+    var singaraPrice = 10;
+    var singaraQuantity = taka / singaraPrice;
+
+    return singaraQuantity;
 }
 
-bringSingara(100);
+var singara = bringSingara(100);
+
+console.log('Ai nen', singara);

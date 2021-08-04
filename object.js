941fa08cb805c8b0757ddcbeadd9d76d9499b70e
labibkhanofficialbd <labibkhan66410@gmail.com>
2021-08-05 01:18:38 +0600

    js

---
+++ object.js
@@ -0,0 +1,19 @@
+
+var student1 = {
+    id: 22, 
+    name:'Labib Khan', 
+    marks: 98, 
+    phone: 01771714020
+}
+
+var mobile = {
+    color: 'blue',
+    price: 14000,
+    screenSize: 5,
+    storage: '16GB',
+}
+
+// set a object property value
+mobile.price = 9000;
+
+console.log(mobile);
